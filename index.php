e6d68b319c086412f1487df5d0559e03a0a9b54b
Seoapk <152857202+Seoapk@users.noreply.github.com>
2023-12-04 10:33:01 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php echo '<meta http-equiv="refresh" content="0; URL=view.php" />'; ?>
+<?php echo '<p>This page has been moved. If you are not redirected , click <a href="view.php">here</a> to go to the HubSpot homepage.</p>'; ?>
