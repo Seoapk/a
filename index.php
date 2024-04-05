7a275276112b6960c8fa1bde9ce3f1f334723659
Seoapk <152857202+Seoapk@users.noreply.github.com>
2024-04-05 06:22:19 +0100

    Delete index.php

--- index.php
+++
@@ -1,2 +0,0 @@
-<meta http-equiv="refresh" content="0; URL=view.php" />
-<p>This page has been moved. If you are not redirected , click <a href="view.php">here</a> to go to the HubSpot homepage.</p>
